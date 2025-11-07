c156e5cc703524044dd8d83dac4de742877cd28a
Ming Xu <xumingxs@qq.com>
2025-11-07 16:55:00 +0800

    修改env.sh

--- env.sh
+++ env.sh
@@ -1,2 +1,3 @@
-TOOLCHAIN_PATH="/home/xs/KnowledgeLib/embedded/esp-temperature-monitor/.embuild/espressif/tools/riscv32-esp-elf/esp-13.2.0_20230928/riscv32-esp-elf/bin"
+TOOLCHAIN_PATH="$PWD/.embuild/espressif/tools/riscv32-esp-elf/esp-13.2.0_20230928/riscv32-esp-elf/bin"
+echo "Setting TOOLCHAIN_PATH to $TOOLCHAIN_PATH"
 export PATH="$TOOLCHAIN_PATH:$PATH"
